032726cfbc855bd76028080264b6cd1fc67fd2ee
orozcoding <orozcoding@gmail.com>
2021-12-31 05:51:11 -0400

    complete basic check function

--- js/backup/testeo2 copy.js
+++ js/backup/testeo2 copy.js
@@ -1,3 +1,6 @@
+const slpPrice = document.querySelector("#slp-precio");
+const slpInput = document.querySelector("#slp-input");
+const slpBalance = document.querySelector("#slp-balance");
 const bcoinPrice = document.querySelector("#bcoin-precio");
 const bcoinInput = document.querySelector("#bcoin-input");
 const bcoinBalance = document.querySelector("#bcoin-balance");
@@ -7,7 +10,11 @@ const atlasBalance = document.querySelector("#atlas-balance");
 const sumaBalance = document.querySelector('#suma-balance');
 let num;
 
-
+let slpData = fetch("https://api.coingecko.com/api/v3/simple/price?ids=smooth-love-potion&vs_currencies=usd")
+  .then((response) => response.json())
+  .then((data) => {
+    return data['smooth-love-potion']['usd'];
+  });
 
 let bcoinData = fetch("https://api.coingecko.com/api/v3/simple/price?ids=bomber-coin&vs_currencies=usd")
   .then((response) => response.json())
@@ -27,7 +34,7 @@ async function printPrice(token, box) {
   box.innerHTML = Number(a);
 };
 
-
+printPrice(slpData, slpPrice);
 printPrice(bcoinData, bcoinPrice);
 printPrice(atlasData, atlasPrice);
 
@@ -40,6 +47,9 @@ function updatePrice(price, input, balance, label) {
   updateTotalBalance()
 }
 
+slpInput.addEventListener('keyup', () => {
+  updatePrice(slpPrice, slpInput, slpBalance, 'slp');
+});
 
 bcoinInput.addEventListener('keyup', () => {
   updatePrice(bcoinPrice, bcoinInput, bcoinBalance, 'bcoin');
@@ -58,11 +68,23 @@ function cargarDatos(label, input, price, balance) {
     num = price.innerHTML * input.value;
     balance.innerHTML = '$' + Math.round(((num) + Number.EPSILON) * 100) / 100;
   }
-};
+}
 
-
+cargarDatos('slp', slpInput, slpPrice, slpBalance);
 cargarDatos('bcoin', bcoinInput, bcoinPrice, bcoinBalance);
 cargarDatos('atlas', atlasInput, atlasPrice, atlasBalance);
+
+
+if (localStorage.getItem('slp-input')) {
+  if (slpPrice.innerHTML == 'cargando...') {
+    slpBalance.innerHTML = 'calculando...'
+    setTimeout(() => {
+      cargarDatos('slp', slpInput, slpPrice, slpBalance);
+    }, 2000)
+  }
+} else {
+  slpBalance.innerHTML = 'Ingresa tus tokens';
+};
 
 
 if (localStorage.getItem('atlas-input')) {
@@ -73,7 +95,7 @@ if (localStorage.getItem('atlas-input')) {
     }, 2000)
   }
 } else {
-  atlasBalance.innerHTML = 'Ingresa tus tokens';
+  slpBalance.innerHTML = 'Ingresa tus tokens';
 };
 
 
@@ -113,7 +135,7 @@ function updateTotalBalance() {
 
 const btnReset = document.getElementById('btn-reset');
 
-function resetAll() {
+function resetAll(){
   slpInput.value = null;
   bcoinInput.value = null;
   atlasInput.value = null;
@@ -140,80 +162,27 @@ btnRefresh.addEventListener('click', () => {
 const slp = {
   content: `<div id="slp-titulo" class="coin-title">SLP</div>
   <div id="slp-section" class="coin-usd">Precio USD:</div>
-  <div>$<div id="slp-precio" class="coin-precio"></div></div>
+  <div id="slp-precio" class="coin-precio"></div>
   <input id="slp-input" class="coin-input" type="number" onwheel="this.blur()">
   <div id="slp-balance" class="coin-balance">Ingresa tus tokens</div>`
 };
 
-const coinsContainer = document.getElementById('coins-container');
+// <div id="slp-container" class="coin-container d-flex">
 
-const cbSlp = document.getElementById('cb-slp');
-if(cbSlp.checked = false){
-  localStorage.setItem('slp-check', 'no');
-};
+  const coinsContainer = document.getElementById('coins-container');
 
-cbSlp.addEventListener('change', () => {
-  if (cbSlp.checked) {
-    let coinSlp = document.createElement('div');
-    coinSlp.setAttribute('id', 'slp-container');
-    coinSlp.classList.add('coin-container', 'd-flex');
-    coinSlp.innerHTML = slp.content;
-    coinsContainer.appendChild(coinSlp);
-    localStorage.setItem('slp-check', 'yes');
-    location.reload();
+  const cbSlp = document.getElementById('cb-slp');
 
-  } else {
-    localStorage.setItem('slp-input', null);
-    localStorage.setItem('slpSum', '0');
-    updateTotalBalance();
-    coinSlp = document.getElementById('slp-container');
-    coinSlp.remove();
-    localStorage.setItem('slp-check', 'no');
-    location.reload();
-
-  }
-});
-
-
-if(localStorage.getItem('slp-check') == 'yes'){
-  cbSlp.checked = true;
-  let coinSlp = document.createElement('div');
-  coinSlp.setAttribute('id', 'slp-container');
-  coinSlp.classList.add('coin-container', 'd-flex');
-  coinSlp.innerHTML = slp.content;
-  coinsContainer.appendChild(coinSlp);
-  localStorage.setItem('slp-check', 'yes');
-
-  const slpPrice = document.querySelector("#slp-precio");
-  const slpInput = document.querySelector("#slp-input");
-  const slpBalance = document.querySelector("#slp-balance");
-
-  let slpData = fetch("https://api.coingecko.com/api/v3/simple/price?ids=smooth-love-potion&vs_currencies=usd")
-    .then((response) => response.json())
-    .then((data) => {
-      return data['smooth-love-potion']['usd'];
-    });
-
-  printPrice(slpData, slpPrice);
-
-  slpInput.addEventListener('keyup', () => {
-    updatePrice(slpPrice, slpInput, slpBalance, 'slp');
-  });
-
-  cargarDatos('slp', slpInput, slpPrice, slpBalance);
-
-  cargarTotalBalance();
-
-  if (localStorage.getItem('slp-input')) {
-    if (slpPrice.innerHTML == 'cargando...') {
-      slpBalance.innerHTML = 'calculando...'
-      setTimeout(() => {
-        cargarDatos('slp', slpInput, slpPrice, slpBalance);
-      }, 2000)
+  cbSlp.addEventListener('change', () => {
+    if(this.checked){
+      let coinSlp = document.createElement('div');
+      coinSlp.setAttribute('id', 'slp-container');
+      coinSlp.classList.add('coin-container d-flex');
+      coinSlp.innerHTML = slp.content;
+      coinsContainer.appendChild(coinSlp);
     }
-  } else {
-    slpBalance.innerHTML = 'Ingresa tus tokens';
-  };
+  })
 
-};
+
+
 
